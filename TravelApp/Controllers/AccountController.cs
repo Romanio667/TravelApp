c9d0010bf909ca4fbfeefc0fe96a9bf796d32291
Roman Miroshnichenko <roma11092000@gmail.com>
2024-02-10 14:59:44 +0700

    Login page created

---
+++ TravelApp/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TravelApp.Data;
+using TravelApp.Models;
+
+namespace TravelApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signManager;
+        private readonly ApplicationDbContext _context;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _context = context;
+            _signManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if(user != null)
+            {
+                //User is found, check password
+                var passwordCheck = await _userManager.CheckPasswordAsync(user,loginViewModel.Password);
+                if(passwordCheck)
+                {
+                    //Password correct, sign in
+                    var result = await _signManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Trip");
+                    }
+                }
+                //Password is incorrect
+                TempData["Error"] = "Wrong credentials. Please try again";
+                return View(loginViewModel);
+            }
+            //User not found
+            TempData["Error"] = "Wrong credentials. Please try again";
+            return View(loginViewModel);
+        }
+    }
+}
